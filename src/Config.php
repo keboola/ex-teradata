4bbe0c988103e7fbc86b4afd419b941d4934e513
David Střítežský <stritezskyd@gmail.com>
2018-08-31 14:21:32 +0200

    Remove unused Config methods

--- src/Config.php
+++ src/Config.php
@@ -8,18 +8,4 @@ use Keboola\Component\Config\BaseConfig;
 
 class Config extends BaseConfig
 {
-    public function getHost() : string
-    {
-        return $this->getValue(['parameters', 'host']);
-    }
-
-    public function getUser() : string
-    {
-        return $this->getValue(['parameters', 'user']);
-    }
-
-    public function getPassword() : string
-    {
-        return $this->getValue(['parameters', '#password']);
-    }
 }
