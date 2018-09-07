d12219840903ef966940310dd100ea53812496e6
David Střítežský <stritezskyd@gmail.com>
2018-09-07 08:06:56 +0200

    Action getTables (#17)
    * Init getTables action
    
    * Status must be `success` - `ok` is not OK enough
    
    * Add ssh node
    
    * Add test for getTables action
    
    * Validate database in getTables action
    
    * Refactoring

--- src/ActionComponent.php
+++ src/ActionComponent.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Keboola\ExTeradata;
 
+use Dibi\Connection;
 use Keboola\Component\BaseComponent;
 use Keboola\ExTeradata\Config\ActionComponent\Config;
 use Keboola\ExTeradata\Config\ActionComponent\ConfigDefinition;
 use Keboola\ExTeradata\Factories\ConnectionFactory;
+use Keboola\ExTeradata\Response\Column;
+use Keboola\ExTeradata\Response\Table;
 
 class ActionComponent extends BaseComponent
 {
@@ -30,8 +33,10 @@ class ActionComponent extends BaseComponent
 
         switch ($config->getAction()) {
             case 'testConnection':
-                $connection->query("SELECT 1");
-                print json_encode(['status' => 'ok'], JSON_PRETTY_PRINT);
+                $this->testConnection($connection);
+                break;
+            case 'getTables':
+                $this->getTables($connection, $config->getDatabase());
                 break;
         }
     }
@@ -44,5 +49,52 @@ class ActionComponent extends BaseComponent
     protected function getConfigDefinitionClass(): string
     {
         return ConfigDefinition::class;
+    }
+
+    private function testConnection(Connection $connection): void
+    {
+        $connection->query("SELECT 1");
+        print json_encode(['status' => 'success'], JSON_PRETTY_PRINT);
+    }
+
+    private function getTables(Connection $connection, string $database): void
+    {
+        print json_encode(
+            [
+                'status' => 'success',
+                'tables' => $this->getTablesResponse($connection, $database),
+            ],
+            JSON_PRETTY_PRINT
+        );
+    }
+
+    private function getTablesResponse(Connection $connection, string $database): array
+    {
+        $tables = $connection->query("SELECT * FROM dbc.tables WHERE DatabaseName=?", $database)->fetchAll();
+        $columns = $connection->query("SELECT * FROM dbc.columns WHERE DatabaseName=?", $database)->fetchAll();
+
+        $tableResponse = [];
+        foreach ($tables as $table) {
+            $tableResponse[] = new Table(
+                trim($table['DatabaseName']),
+                trim($table['TableName']),
+                $this->getTableColumnsResponse(trim($table['TableName']), $columns)
+            );
+        }
+
+        return $tableResponse;
+    }
+
+    private function getTableColumnsResponse(string $table, array $columns): array
+    {
+        $columnsResponse = [];
+        foreach ($columns as $column) {
+            if (trim($column['TableName']) === $table) {
+                $columnsResponse[] = new Column(
+                    trim($column['ColumnName'])
+                );
+            }
+        }
+        return $columnsResponse;
     }
 }
