051dda363f9384099d9b19beeddf02067bccc914
David Střítežský <stritezskyd@gmail.com>
2018-09-11 22:39:58 +0200

    Refactor ExceptionHandler

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -4,61 +4,61 @@ declare(strict_types=1);
 
 namespace Keboola\ExTeradata;
 
-use Keboola\Component\UserException;
-
 class ExceptionHandler
 {
+    /** @var MessageTransformation[] */
+    private $messageTransformations = [];
+
+    public function __construct()
+    {
+        $this->messageTransformations = [
+            new MessageTransformation(
+                '~Can\'t assign requested address 08S01~',
+                'Cannot assign requested address.'
+            ),
+            new MessageTransformation(
+                '~Network is unreachable 08S01~',
+                'Network is unreachable.'
+            ),
+            new MessageTransformation(
+                '~The Teradata server is not accepting connections 08004~',
+                'The Teradata server is not accepting connections.'
+            ),
+            new MessageTransformation(
+                '~No response received when attempting to connect to the Teradata server S1000~',
+                'No response received when attempting to connect to the Teradata server.'
+            ),
+            new MessageTransformation(
+                '~The Teradata server can\'t currently be reached over this network~',
+                'The Teradata server can\'t currently be reached over this network.'
+            ),
+            new MessageTransformation(
+                '~The UserId, Password or Account is invalid.~',
+                'The User or Password is invalid.'
+            ),
+            new MessageTransformation(
+                '~Object \'([^\']+)\.([^\']+)\' does not exist.~',
+                'Table "%s" does not exist in database "%s".',
+                [2, 1]
+            ),
+            new MessageTransformation(
+                '~Database \'([^\']+)\' does not exist.~',
+                'Database "%s" does not exist.',
+                [1]
+            ),
+        ];
+    }
+
     public function createException(\Throwable $exception): \Throwable
     {
-        if (preg_match(
-            '~Can\'t assign requested address 08S01~',
-            $exception->getMessage()
-        )) {
-            return new UserException('Cannot assign requested address.');
-        } elseif (preg_match(
-            '~Network is unreachable 08S01~',
-            $exception->getMessage()
-        )) {
-            return new UserException('Network is unreachable.');
-        } elseif (preg_match(
-            '~The Teradata server is not accepting connections 08004~',
-            $exception->getMessage()
-        )) {
-            return new UserException('The Teradata server is not accepting connections.');
-        } elseif (preg_match(
-            '~No response received when attempting to connect to the Teradata server S1000~',
-            $exception->getMessage()
-        )) {
-            return new UserException('No response received when attempting to connect to the Teradata server.');
-        } elseif (preg_match(
-            '~The Teradata server can\'t currently be reached over this network~',
-            $exception->getMessage()
-        )) {
-            return new UserException('The Teradata server can\'t currently be reached over this network.');
-        } elseif (preg_match(
-            '~The UserId, Password or Account is invalid.~',
-            $exception->getMessage()
-        )) {
-            return new UserException('The User or Password is invalid.');
-        } elseif (preg_match(
-            '~Object \'([^\']+)\.([^\']+)\' does not exist.~',
-            $exception->getMessage(),
-            $matches
-        )) {
-            return new UserException(sprintf(
-                'Table "%s" does not exist in database "%s".',
-                $matches[2],
-                $matches[1]
-            ));
-        } elseif (preg_match(
-            '~Database \'([^\']+)\' does not exist.~',
-            $exception->getMessage(),
-            $matches
-        )) {
-            return new UserException(sprintf(
-                'Database "%s" does not exist.',
-                $matches[1]
-            ));
+        foreach ($this->messageTransformations as $messageTransformation) {
+            if (preg_match(
+                $messageTransformation->getPattern(),
+                $exception->getMessage(),
+                $matches
+            )) {
+                return $messageTransformation->getUserException($matches);
+            }
         }
 
         return $exception;
