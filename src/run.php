647fef8e9c5ae08c2ecc8d4e394f4686b2349190
David Střítežský <stritezskyd@gmail.com>
2018-10-17 17:07:21 +0200

    Always debug

--- src/run.php
+++ src/run.php
@@ -9,37 +9,8 @@ use Keboola\ExTeradata\CoreComponent;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$logger = new Logger();
-try {
-    $dataDir = getenv('KBC_DATADIR') ?? '/data/';
-    $configJson = file_get_contents($dataDir . DIRECTORY_SEPARATOR . 'config.json');
-    $action = json_decode((string) $configJson, true)['action'] ?? 'run';
-
-    switch ($action) {
-        case 'run':
-            $app = new CoreComponent($logger);
-            break;
-        default:
-            $app = new ActionComponent($logger);
-    }
-    $app->run();
-    exit(0);
-} catch (UserException $e) {
-    $logger->error($e->getMessage());
-    exit(1);
-} catch (\Throwable $e) {
-    $logger->critical(
-        get_class($e) . ':' . $e->getMessage(),
-        [
-            'errFile' => $e->getFile(),
-            'errLine' => $e->getLine(),
-            'errCode' => $e->getCode(),
-            'errTrace' => $e->getTraceAsString(),
-            'errPrevious' => $e->getPrevious() ? get_class($e->getPrevious()) : '',
-        ]
-    );
-
-
+function printDebug(): void
+{
     print PHP_EOL;
     print PHP_EOL;
     $traceFile = '/usr/odbcusr/trace.log';
@@ -71,6 +42,39 @@ try {
     } else {
         print "Debug File does not exist." . PHP_EOL;
     }
+}
 
+$logger = new Logger();
+try {
+    $dataDir = getenv('KBC_DATADIR') ?? '/data/';
+    $configJson = file_get_contents($dataDir . DIRECTORY_SEPARATOR . 'config.json');
+    $action = json_decode((string) $configJson, true)['action'] ?? 'run';
+
+    switch ($action) {
+        case 'run':
+            $app = new CoreComponent($logger);
+            break;
+        default:
+            $app = new ActionComponent($logger);
+    }
+    $app->run();
+    printDebug();
+    exit(0);
+} catch (UserException $e) {
+    $logger->error($e->getMessage());
+    printDebug();
+    exit(1);
+} catch (\Throwable $e) {
+    $logger->critical(
+        get_class($e) . ':' . $e->getMessage(),
+        [
+            'errFile' => $e->getFile(),
+            'errLine' => $e->getLine(),
+            'errCode' => $e->getCode(),
+            'errTrace' => $e->getTraceAsString(),
+            'errPrevious' => $e->getPrevious() ? get_class($e->getPrevious()) : '',
+        ]
+    );
+    printDebug();
     exit(2);
 }
