0011137a234f83978ae24dad05c416fc3b889a36
David Střítežský <stritezskyd@gmail.com>
2018-10-17 15:14:58 +0200

    Debug ODBC

--- src/run.php
+++ src/run.php
@@ -38,5 +38,39 @@ try {
             'errPrevious' => $e->getPrevious() ? get_class($e->getPrevious()) : '',
         ]
     );
+
+
+    print PHP_EOL;
+    print PHP_EOL;
+    $traceFile = '/usr/odbcusr/trace.log';
+    if (file_exists($traceFile)) {
+        $trace = file_get_contents($traceFile);
+        print "Trace File:" . PHP_EOL;
+        print $trace;
+        print PHP_EOL;
+    } else {
+        print "Trace File does not exist." . PHP_EOL;
+    }
+
+    $traceFile = '/usr/odbcusr/trace.log';
+    if (file_exists($traceFile)) {
+        $trace = file_get_contents($traceFile);
+        print "Trace File:" . PHP_EOL;
+        print $trace;
+        print PHP_EOL;
+    } else {
+        print "Trace File does not exist." . PHP_EOL;
+    }
+
+    $debugFile = '/usr/odbcusr/debug.log';
+    if (file_exists($debugFile)) {
+        $debug = file_get_contents($debugFile);
+        print "Debug File:" . PHP_EOL;
+        print $debug;
+        print PHP_EOL;
+    } else {
+        print "Debug File does not exist." . PHP_EOL;
+    }
+
     exit(2);
 }
