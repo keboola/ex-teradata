31e38ce01c530bebd8806f2fe31daf71cfd49cc7
David Střítežský <stritezskyd@gmail.com>
2018-10-18 15:28:01 +0200

    Save debug and trace as file

--- src/Debug.php
+++ src/Debug.php
@@ -6,8 +6,12 @@ namespace Keboola\ExTeradata;
 
 class Debug
 {
-    public static function print(): void
+    public static function print(string $datadir): void
     {
+        if (!is_dir($datadir . '/out/files')) {
+            mkdir($datadir . '/out/files', 0777, true);
+        }
+
         $traceFile = '/usr/odbcusr/trace.log';
         if (file_exists($traceFile)) {
             print PHP_EOL;
@@ -16,6 +20,8 @@ class Debug
             print "Trace File:" . PHP_EOL;
             print $trace;
             print PHP_EOL;
+
+            file_put_contents($datadir . '/out/files/trace.log', $trace);
         } else {
             // print "Trace File does not exist." . PHP_EOL;
         }
@@ -26,6 +32,8 @@ class Debug
             print "Debug File:" . PHP_EOL;
             print $debug;
             print PHP_EOL;
+
+            file_put_contents($datadir . '/out/files/debug.log', $debug);
         } else {
             // print "Debug File does not exist." . PHP_EOL;
         }
